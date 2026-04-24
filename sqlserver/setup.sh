b687dd18a3aa3d65730aea7f0c121563ccf161f1
Minnieyuu <152836466+Minnieyuu@users.noreply.github.com>
2026-04-24 16:23:12 +0800

    0424

--- sqlserver/setup.sh
+++ sqlserver/setup.sh
@@ -1,6 +1,6 @@
 # 等待 SQL Server 啟動（給它 30 秒）
 echo "等待 SQL Server 啟動..."
-sleep 30s
+sleep 1ms
 
 # 執行初始化 SQL
 /opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -No -d master -i /usr/config/init.sql
